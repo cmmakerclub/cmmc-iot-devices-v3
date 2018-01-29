28006dc68986b31a08bac9bffac8b41f10037ec1
Compiler Exe <compilerexe@gmail.com>
2018-01-29 16:42:04 +0700

    Add loading.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
-import Navbar from './components/Navbar'
 import Connection from './components/Connection.js'
 import store from './flux/Store'
-import uuid from 'uuid'
-import _ from 'underscore'
-import logo from './assets/cmmc-logo.png'
+import loading from './assets/loading-2.gif'
 import Devices from './components/Devices'
 
 class App extends Component {
@@ -13,7 +10,8 @@ class App extends Component {
     super(props)
     this.state = {
       messages: [],
-      connection: false
+      connection: false,
+      hiddenLoading: 'none'
     }
 
     store.addListener(() => {
@@ -28,49 +26,29 @@ class App extends Component {
         messages: devices,
         connection: store.state.connection
       })
+
+      if (this.state.connection && this.state.messages.length === 0) {
+        this.setState({hiddenLoading: 'block'})
+      } else {
+        this.setState({hiddenLoading: 'none'})
+      }
+
     })
   }
 
   render () {
-
-    const hiddenWhenConnectingFail = !this.state.connection ? 'none' : 'block'
-    const hiddenWhenConnectingSuccess = this.state.connection ? 'none' : 'block'
-
     return (
       <div className="container">
-        {/*<Navbar/>*/}
         <div className="row" style={{marginTop: 20}}>
-          {/*<div className="col-12 col-md-3" style={{marginTop: 20, display: hiddenWhenConnectingSuccess}}>*/}
           <div className="col-12 col-md-3">
             <Connection/>
           </div>
-          {/*<div className='col-12' style={{marginTop: 20, display: hiddenWhenConnectingFail}}>*/}
           <div className='col-12 col-md-9'>
             <div className="form-group">
-
-              {/*<div className="row">*/}
-              {/*<div className="col-12 col-md-2 text-center">*/}
-              {/*<img src={logo} style={{height: 30}} alt=""/>*/}
-              {/*</div>*/}
-              {/*<div className="col text-right">*/}
-              {/*<h3>*/}
-              {/*Connection <i className={this.state.connection ? 'fa fa-circle text-success' : 'block'}/>*/}
-              {/*</h3>*/}
-              {/*</div>*/}
-              {/*</div>*/}
-
-              {/*<hr/>*/}
-
-              {/*<div className="form-group">*/}
-              {/**/}
-              {/*<h3>*/}
-              {/*<i className={this.state.connection && 'fa fa-circle text-success'}/>&ensp;*/}
-              {/*{this.state.connection && 'Devices'}*/}
-              {/*</h3>*/}
-              {/*</div>*/}
-
+              <div style={{display: this.state.hiddenLoading}} className='text-right'>
+                Please wait a few minute&ensp;<img src={loading} style={{width: 30}} alt=""/>
+              </div>
               <Devices/>
-
             </div>
           </div>
         </div>
