2bb0e9214004c91882459b41c7d547204e6edac8
Compiler Exe <compilerexe@gmail.com>
2018-01-29 17:41:19 +0700

    Add fadeIn, fadeOut for connection component.

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -3,6 +3,7 @@ import Dispatcher from '../flux/Dispatcher'
 import TypeActions from '../flux/Constants'
 import logo from '../assets/cmmc-logo.png'
 import store from '../flux/Store'
+import './styles.css'
 
 export default class Connection extends Component {
 
@@ -52,6 +53,7 @@ export default class Connection extends Component {
       <div className="form-group">
         <div className="card">
           <div className="card-body">
+
             <form>
               <div className="form-group">
                 <img src={logo} style={{height: 30}} alt=""/>
@@ -60,47 +62,56 @@ export default class Connection extends Component {
                 {this.state.connection ? 'Connection' : 'Waiting for connection'}&ensp;
                 <i className={this.state.connection ? 'fa fa-circle text-success' : 'fa fa-circle text-danger'}/>
               </h6>
-              <div className="form-group">
-                Host
-                <input type="text" className='form-control' defaultValue={'mqtt.cmmc.io'}
-                       onChange={e => this.setState({host: e.target.value})} disabled={this.state.disableField}/>
-              </div>
-              <div className="form-group">
-                Port
-                <input type="text" className='form-control' defaultValue={9001}
-                       onChange={e => this.setState({port: e.target.value})} disabled={this.state.disableField}/>
-              </div>
-              <div className="form-group">
-                ClientID
-                <input type="text" className='form-control' defaultValue={this.state.clientId}
-                       disabled={this.state.disableField}/>
-              </div>
-              <div className="form-group">
-                Username
-                <input type="text" className='form-control' onChange={e => this.setState({username: e.target.value})}
-                       disabled={this.state.disableField}/>
-              </div>
-              <div className="form-group">
-                Password
-                <input type="password" className='form-control'
-                       onChange={e => this.setState({password: e.target.value})} disabled={this.state.disableField}/>
-              </div>
-              <div className="form-group">
-                Topic
-                <input type="text" className='form-control' defaultValue={this.state.topic}
-                       onChange={e => this.setState({topic: e.target.value})} disabled={this.state.disableField}/>
-              </div>
-              <div className="form-group" style={{display: hiddenWhenConnectingSuccess}}>
-                <button type='button' className='btn btn-success' style={{width: '100%'}}
-                        onClick={e => this.handleOnConnect(e)}>
-                  <i className='fa fa-globe'/> Connect
-                </button>
-              </div>
+
               <div className="form-group" style={{display: hiddenWhenConnectingFail}}>
                 <button type='button' className='btn btn-danger' style={{width: '100%'}}
                         onClick={e => this.handleOnDisconnect(e)}>Disconnect
                 </button>
               </div>
+
+              <div className={this.state.connection ? 'fadeOut' : 'fadeIn'}>
+
+                <div className="form-group">
+                  Host
+                  <input type="text" className='form-control' defaultValue={'mqtt.cmmc.io'}
+                         onChange={e => this.setState({host: e.target.value})} disabled={this.state.disableField}/>
+                </div>
+                <div className="form-group">
+                  Port
+                  <input type="text" className='form-control' defaultValue={9001}
+                         onChange={e => this.setState({port: e.target.value})} disabled={this.state.disableField}/>
+                </div>
+                <div className="form-group">
+                  ClientID
+                  <input type="text" className='form-control' defaultValue={this.state.clientId}
+                         disabled={this.state.disableField}/>
+                </div>
+                <div className="form-group">
+                  Username
+                  <input type="text" className='form-control'
+                         onChange={e => this.setState({username: e.target.value})}
+                         disabled={this.state.disableField}/>
+                </div>
+                <div className="form-group">
+                  Password
+                  <input type="password" className='form-control'
+                         onChange={e => this.setState({password: e.target.value})}
+                         disabled={this.state.disableField}/>
+                </div>
+                <div className="form-group">
+                  Topic
+                  <input type="text" className='form-control' defaultValue={this.state.topic}
+                         onChange={e => this.setState({topic: e.target.value})} disabled={this.state.disableField}/>
+                </div>
+                <div className="form-group" style={{display: hiddenWhenConnectingSuccess}}>
+                  <button type='button' className='btn btn-success' style={{width: '100%'}}
+                          onClick={e => this.handleOnConnect(e)}>
+                    <i className='fa fa-globe'/> Connect
+                  </button>
+                </div>
+
+              </div>
+
             </form>
           </div>
         </div>
