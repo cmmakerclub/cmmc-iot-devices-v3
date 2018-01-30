e2dc3dadf101cb0c91a3cf94f564612f46d0ddea
Compiler Exe <compilerexe@gmail.com>
2018-01-30 14:18:21 +0700

    Add mockup design filter.

--- src/App.js
+++ src/App.js
@@ -40,6 +40,40 @@ class App extends Component {
     return (
       <div className="container">
         <div className="row" style={{marginTop: 20}}>
+          <div className="col-12 col-md-9 offset-md-3" style={{marginBottom: 20}}>
+            <div className="from-group">
+              <div className="card">
+                <div className="card-body">
+
+                  <div className="row">
+                    <div className="col-12 col-md-10">
+                      <input type="text" className='form-control' placeholder='Filter device by name ...'/>
+                    </div>
+                    <div className="col-12 col-md-2 text-right">
+                      <div className="dropdown">
+                        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          <i className='fa fa-filter'/>&nbsp;
+                          Filter
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                          <a className="dropdown-item" href="#">
+                            <i className='fa fa-circle text-success'/>&ensp;
+                            Online
+                          </a>
+                          <a className="dropdown-item" href="#">
+                            <i className='fa fa-circle text-secondary'/>&ensp;
+                            Offline
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="col-12 col-md-3">
             <Connection/>
           </div>
